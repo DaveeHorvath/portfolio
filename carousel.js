c4f6d450b7a712b4bb6cf87255765c834f1d0477
dhorvath <david.horvath@gmx.de>
2024-05-14 16:00:47 +0300

    initial

---
+++ carousel.js
@@ -0,0 +1,142 @@
+        data = [
+            {
+                "title": "Fdf",
+                "desc": "Fdf is one of the graphics oriented project of the 42 network. \
+                The problem you need to solve is about rendering a wireframe of a 3d map. \
+                It requires some mathematical background or research, but the results speak for themselves.\
+                It uses the schools own graphics library based on openGL but with limited functionality, where you are only able to place 1 pixel at a time. \
+                The bonus parts of the projects include movement, rotation and in my case some automatic rotation. \
+                Check the github page for more information about the learning and discovery process.\
+                I also had a presentation on this project, which is available to Hive students.",
+                "gh": "https://github.com/daveekun/fdf_",
+                "imgurl": "./fdf.png"
+            },
+            {
+                "title": "PushSwap",
+                "desc": "Numbers are fun. Sorting them is even funner, and sorting algorithms are a \
+                part of many coders journey. This is a project for school where you need to come up \
+                with a sorting algo for a stack with limited operations like rotations and pushing numbers \
+                between 2 given circular stacks. The project itself is really nice, especially the \
+                solving algorithm part which has many small challenges, mostly about optimising for speed and \
+                trying to achive the least amount of moves possible. The result is really satisfying, even as \
+                its not at 100% optimisation.",
+                "gh": "https://github.com/daveekun/pushswap",
+                "imgurl": ""
+            },
+            {
+                "title": "RayTracer",
+                "desc": "Raytracing is the art of simulating light rays to display 3d objects and scenes\
+                This project was a group effort with Bobbie Syväsalmi, a fellow Hive student. This is our miniRT project \
+                which can be used to (within the limitations) display simple scenes with multiple objects, light sources \
+                with a full PHONG lighting model based purely on math. It can render cones, cylinders, planes and spheres \
+                sadly real time updates where outside of the projects scope, and the limitations of mlx42 \
+                didnt allow for this. It is a topic to revisit, and to improve on, as it is one of the most fun project I have been part of.",
+                "gh": "https://github.com/Moridar/minirt",
+                "imgurl": "./minirt.png"
+            },
+            {
+                "title": "Minishell",
+                "desc": "A shell is a tool to interact with computers by typing commands into a command line\
+                and is a great way to interact with systems. Writting a shell is not a small task, especially \
+                in C, while trying to mimic Bash (one of the most used shells). This has been the first big group \
+                project at Hive I have been part of. It came with the usual challenges of collaboration added on top \
+                of my task being the parser and execution part of the project. After the user types anything I was responsible \
+                for figuring out if it was correct and if the computer was able to execute it. Pipes, redirections and quoting \
+                where a big hurdle, but writting an Abstract Syntax Tree to allow for command chaining proved to be quite hard but rewarding. \
+                My partner in crime on this project was Ilmari Välimäki. #putchar",
+                "gh": "https://github.com/daveekun/minihell",
+                "imgurl": "./minishell.png"
+            },
+            {
+                "title": "Writeups",
+                "desc": "Writeups are closely associated with CTFs or Capture The Flag events and platforms \
+                CTFs are challenges that involve critical thinking and solving problems in a creative usually \
+                outside-the-box ideas. Usually these are stepping stones for people wanting to get into the \
+                cybersecurity industry, as a lot of these include potentionally vulnerable machines that we need \
+                to exploit to attain said flags. Writeups serve as proof of a \"hackers\" ability to get into systems \
+                done in a controlled safe and legal enviroment. These are my writeups for the HackTheBox machines \
+                and other CTFs I am participating in.",
+                "gh": "https://github.com/daveekun/Writeups",
+                "imgurl": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fca.trustpilot.com%2Freview%2Fhackthebox.com&psig=AOvVaw3oux2Dz7jPMgLEPxNTpuKm&ust=1715788032428000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDz-rWWjYYDFQAAAAAdAAAAABAE"
+            },
+            {
+                "title": "Game - work in progress",
+                "desc": "A work in progress tower defense game with another Hive Student. \
+                Unity is the current game engine, development is in really early stages.",
+                "gh": "https://github.com/daveekun/tdgame",
+                "imgurl": ""
+            },
+            {
+                "title": "Python",
+                "desc": "Python is a great language to learn the basics of coding \
+                later it becomes a great multitool for automation, cybersecurity and more \
+                I mostly use if for one if its great libraries pwntools. This helps with bufferoverflows \
+                and other connections for CTFs. Python helped me get a great score on the school leaving exam \
+                for computer science, but I have also written a simple vision algorithm/equasion solver for \
+                the MaTech competition in this, to predict if a basketball will land in a hoop or not by taking \
+                a couple of pictures of its path. I am also happy to teach people with python as its really \
+                intuitive for beginners and almost reads like english. (if I find the source, will upload to gh)",
+                "gh": "#",
+                "imgurl": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ntuclearninghub.com%2F-%2Fcourse%2Ffundamentals-of-python-programming-sf&psig=AOvVaw3iMzWvfk1h1piiBpwvO_F3&ust=1715787465399000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiDtKiUjYYDFQAAAAAdAAAAABAV"
+            },
+            {
+                "title": "C++ Modules",
+                "desc": "Many 42 Network schools require its students to learn C++ \
+                and one of the first steps before starting any big project is learning the basics \
+                as they say \"learn how to walk before you run\". These stepping stones are the \
+                C++ modules, where each of them focus on a topic of Object Oriented Programming \
+                These are great practice and lead to a big project (Webserver in C++) which is a work in \
+                progress.",
+                "gh": "https://github.com/daveekun/cppModules",
+                "imgurl": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2F%40anushkasnawale%2Fcreate-website-using-c-3c06d3381a17&psig=AOvVaw3J7ULwlmAcQ3dQr0y9BVpi&ust=1715787415087000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDnwY-UjYYDFQAAAAAdAAAAABAE"
+            },
+            {
+                "title": "Rendererer - Work in progress",
+                "desc": "After starting to learn C++ and writting our own raytracer the idea of an \
+                actual renderer got recommended to me. This is where I discovered Vulkan, and have been \
+                gradually working on a raytracer/gameengine with it that is a work in progress. \
+                Vulkan provides the tool to reach greatness, but I am only in the first steps of learning it.",
+                "gh": "https://github.com/daveekun/raytracer/tree/thing",
+                "imgurl": ""
+            },
+
+
+        ]
+        let length = data.length
+        let current = 0
+
+        let title = document.getElementById("projecttitle")
+        let desc = document.getElementById("projectdesc")
+        let gh = document.getElementById("projectgh")
+        let pics = document.getElementById("projectpics")
+        let prev = 0;
+
+        function render() {
+            document.getElementById("" + prev).classList.remove("current");
+            document.getElementById("" + current).classList.add("current");
+            title.innerHTML = data[current].title
+            pics.style.backgroundImage = "url("+ data[current].imgurl+")"
+            desc.innerHTML = "<p class=\"projectdesctext\">" + data[current].desc + "</p>"
+            gh.innerHTML = "<a class=\"cardlink\" href=\"" + data[current].gh + "\">" + data[current].gh + "</a>"
+        }
+
+        function set(id) {
+            prev = current
+            current = id
+            render()
+        }
+
+        function left() {
+            prev = current
+            if (current == 0)
+                current = length
+            current = (current - 1) % length;
+            render()
+        }
+
+        function right() {
+            prev = current
+            current = (current + 1) % length;
+            render()
+        }
+        document.addEventListener("DOMContentLoaded", render)
